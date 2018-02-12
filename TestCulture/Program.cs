34daf151ad5a5a3b19dd6ff873b4cbc4e35d83b3
pmlange <pmlange@gmail.com>
2018-02-12 13:14:53 +0100

    initial commit

---
+++ TestCulture/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace TestCulture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CultureInfo info = new CultureInfo("da-DK");
+            CultureInfo.CurrentCulture = info;
+            Console.WriteLine(CultureInfo.CurrentCulture);
+            decimal d = 9.4356M;
+            Console.WriteLine(d);
+            Console.ReadLine();
+
+        }
+    }
+}
